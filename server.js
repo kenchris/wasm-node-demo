c7d601b13ee3a218a840f51b308d4384a5cbc140
Kenneth Christiansen <kenneth.christiansen@gmail.com>
2018-05-22 17:48:38 +0000

    Works now

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const Module = require("./a.out.js");
 const express = require("express");
 const app = express();
 
-app.get('/', async (req, res) => {
-  console.log("ES!");
-  let instance = await Module();
-  res.send(instance._(req.query.n));
+let fib = null;
+Module().then(instance => {
+  fib = instance._f;
+  console.log(fib(27));
+});
+
+app.get("/", async (req, res) => {
+  let n = req.query.n ? req.query.n : 1;
+
+  if (n <= 50) { n = 50 }
+
+  let result = fib(n);
+
+  console.log("result = " + result);
+  res.send(`Fibonacci of ${n} is ${result} - calculated using WebAssembly!`);
 });
 
 app.listen(8080, () => console.log("starting fib fun"));
-
